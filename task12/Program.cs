0630e02d50d8b5e562f923f5f4fbed249564b566
QazbekR <qzrysbekov@gmail.com>
2023-02-18 12:18:14 +0600

    задачи второго семинара q

--- task12/Program.cs
+++ task12/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить,
+//является ли второе число кратным первому. Если второе число не кратно числу первому,
+//то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно;
+
+Console.Clear();
+int num1 = new Random().Next(10, 100);
+int num2 = new Random().Next(1, 10);
+int max = num1 % num2;
+if  (max > 0) {Console.WriteLine($"{num1} не кратно {num2}, остаток {max}");}
+else {Console.WriteLine($"{num1} кратно {num2}");}
+
