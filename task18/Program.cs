a821d3074a02cb2c36a1f4eba49f8d32a7f2e050
QazbekR <qzrysbekov@gmail.com>
2023-02-18 18:51:25 +0600

    второй семинар

---
+++ task18/Program.cs
@@ -0,0 +1,31 @@
+﻿// **Задача 18:**
+
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+Console.WriteLine("ВВедите номер четверти");
+int a = int.Parse(Console.ReadLine());
+switch (a)
+{
+    case 1:{Console.WriteLine("Х > 0 and Y > 0");
+    break;
+    }
+    case 2:{Console.WriteLine("Х < 0 and Y > 0");
+    break;
+    }
+    case 3:{Console.WriteLine("Х < 0 and Y < 0");
+    break;
+    }
+    case 4:{Console.WriteLine("Х > 0 and Y < 0");
+    break;
+    }
+    default:{Console.WriteLine("неправильная четверть");
+    break;
+    }
+}
+/*if (a == 1) {Console.WriteLine("Х > 0 and Y > 0");}
+if (a == 2) {Console.WriteLine("Х < 0 and Y > 0");}
+if (a == 3) {Console.WriteLine("Х < 0 and Y < 0");}
+if (a == 4) {Console.WriteLine("Х > 0 and Y < 0");}
+*/
---
+++ task20/Program.cs
@@ -0,0 +1,22 @@
+﻿// 20 Напишите программу, 
+// которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 2D пространстве.
+
+// A (3,6); B(2, 1)-> 5,09
+// A(7, -5); B(1, -1)-> 7,21
+//sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
+//double s = Math.Sqrt(...);
+
+Console.Clear();
+Console.WriteLine("Введите координату Х первой точки");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Х второй точки");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y второй точки");
+int y2 = int.Parse(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)); // math pow это степень, то есть х1 - х2 и степень 2
+Console.WriteLine($"Точка А ({x1}; {y1}). Точка B ({x2}; {y2})");
+Console.WriteLine($"Расстояние между точками А и В - {distance}");
+
